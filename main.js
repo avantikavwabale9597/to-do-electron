e8f9179ed024c4866be43e6366c904250db19b15
Ava9597 <bonobonoyah1997@gmail.com>
2026-01-09 21:22:29 +0530

    extended size

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-const {app, BrowserWindow} = require ('electron')
-function createWindow(){
-    const w = new BrowserWindow({
-     width:800,
-     height:600,
-     webPreferences:{
-        nodeIntegration:true,
-        contextIsolation:false
-     }   
-    })
-    w.loadFile('index.html')
+const { app, BrowserWindow } = require("electron");
+function createWindow() {
+  const w = new BrowserWindow({
+    width: 800,
+    height: 700,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+  w.loadFile("index.html");
 }
-app.whenReady().then(createWindow)
-app.on('window-all-closed',()=>{
-    if(process.platform!=='darwin'){
-        app.quit()
-    }
-})
+app.whenReady().then(createWindow);
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
